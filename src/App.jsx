8d82cfbc1ca45256cd1babc5ab51bc1162e42b14
Ravi Mali <ravimalakar091@gmail.com>
2026-05-31 17:06:14 +0530

    add auth slice and configure redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-
-
+import { useState, useEffect } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice";
+import { Header, Footer } from "./component";
+import { Outlet } from "react-router-dom";
 function App() {
   
-console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>This is new project </h1>
-    </>
-  )
+const [loading, setLoading] = useState(true);
+const disPatch = useDispatch();
+
+useEffect(() => {
+  authService.getCurrentUser().then((user) => {
+    if (user) {
+      disPatch(login({user}));
+    }else{
+      disPatch(logout());
+    }
+  }).finally(() => {setLoading(false);
+  })
+},[])
+   return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        {/* TODO:  <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
