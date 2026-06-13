0d3d922da48ceb77c453c29e88507844cf85d17b
Ravi Mali <ravimalakar091@gmail.com>
2026-06-13 14:07:07 +0530

    style: update add post page layout and background to match blog theme

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -3,10 +3,20 @@ import { Container, PostForm } from '../components'
 
 function AddPost() {
   return (
-    <div className='py-8'>
+    <div className='min-h-screen bg-gray-50'>
+      <div className='bg-white border-b border-gray-100'>
         <Container>
-            <PostForm />
+          <div className='py-6'>
+            <h1 className='text-2xl font-bold text-gray-900'>Write a New Post</h1>
+            <p className='text-sm text-gray-500 mt-1'>Share your thoughts with the world</p>
+          </div>
         </Container>
+      </div>
+      <Container>
+        <div className='py-8'>
+          <PostForm />
+        </div>
+      </Container>
     </div>
   )
 }
